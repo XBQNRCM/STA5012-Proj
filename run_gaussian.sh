e90a54bc287672b4e56531d781db46735df6c8c4
Huang Z <zijinghuang@link.cuhk.edu.cn>
2026-05-08 20:12:03 +0800

    expand m

--- run_gaussian.sh
+++ run_gaussian.sh
@@ -1,9 +1,10 @@
 # baseline result for gaussian setting
 python run_eval.py \
     --mode gaussian \
-    --maps performer \
-    --m 16 24 32 40 48 56 64 72 80 \
+    --metric both \
+    --maps performer power_performer \
+    --m 16 24 32 40 48 56 64 72 80 88 96 104 112 120 128 136 144 152 160\
     --n-pairs 10000 \
-    --n-trials 100 \
+    --n-trials 5000 \
     --seed 42 \
     --device cuda
--- run_gpt2.sh
+++ run_gpt2.sh
@@ -1,13 +1,28 @@
 # baseline result for gpt2 setting
 python run_eval.py \
     --mode gpt2 \
-    --maps performer \
-    --m 16 24 32 40 48 56 64 72 80 \
+    --metric ker \
+    --maps performer power_performer \
+    --m 16 24 32 40 48 56 64 72 80 88 96 104 112 120 128 136 144 152 160 \
     --n-docs 1000 \
-    --max-length 512 \
+    --max-length 128 \
     --layers 2,4,6,8,10 \
     --heads 2,4,6,8,10 \
     --token-pos -2 \
     --n-trials 5000 \
+    --seed 42 \
+    --device cuda
+
+python run_eval.py \
+    --mode gpt2 \
+    --metric out \
+    --maps performer power_performer \
+    --m 16 24 32 40 48 56 64 72 80 88 96 104 112 120 128 136 144 152 160 \
+    --n-docs 1000 \
+    --max-length 128 \
+    --layers 2,4,6,8,10 \
+    --heads 2,4,6,8,10 \
+    --token-pos -2 \
+    --n-trials 1 \
     --seed 42 \
     --device cuda
